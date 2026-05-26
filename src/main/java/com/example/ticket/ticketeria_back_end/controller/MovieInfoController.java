25f2a3467022a64045c51c7d2d81af92513945f9
Portella <Allysonpalmeirim12@gmail.com>
2026-05-26 17:57:11 -0300

    bug fixes

--- src/main/java/com/example/ticket/ticketeria_back_end/controller/MovieInfoController.java
+++ src/main/java/com/example/ticket/ticketeria_back_end/controller/MovieInfoController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,6 +24,7 @@ public class MovieInfoController {
     private final MoviesService moviesService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public List<MovieEntity>getMovies(){
         return MoviesService.getMovies();
     }
